a29cb2992bc4a5b084aba34094c12ee7a0d32dea
Antoine <antoine.leca@laplateforme.io>
2025-03-25 15:27:09 +0100

    ajout de la structure des pages, header/footer et le template de page avec les require et head de base

---
+++ views/template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Site de recettes">
+    <meta name="keywords" content="Recettes en ligne">
+    <meta name="author" content="Marvin, Antoine">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- liens css -->
+    <link rel="stylesheet" href="/happyMeal/public/css/root.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Shrikhand&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <title>Tasty</title>
+</head>
+<body class="h-screen">
+    <?php require_once(__DIR__ . '/header.php'); ?>
+    <main>
+    
+    </main>
+    <?php require_once(__DIR__ . '/footer.php'); ?>
+</body>
+</html>
